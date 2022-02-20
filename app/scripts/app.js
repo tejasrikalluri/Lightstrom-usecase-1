3c3d157c64e8f8ecf0ad7518c1fbe488a0319ae1
tejasrikalluri <tkalluri@artissol.com>
2022-02-20 13:27:40 +0530

    added remainning function blocks, added parent ticket child ticket condition modified priority validation error block modified stringify to parse

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -65,8 +65,15 @@ function viewRelatedTickets(display_id, updatesFields, map, dropdownMap, priorit
     try {
       var resp = JSON.parse(data.response);
       console.log(resp.ticket)
-      formUpdateBody(display_id, updatesFields, map, dropdownMap, priority, resp, event_data);
-
+      if (!$.isEmptyObject(resp.ticket.related_tickets)) {
+        if ("child_ids" in resp.ticket.related_tickets) {
+          formUpdateBody(resp.ticket.related_tickets.child_ids, updatesFields, map, dropdownMap, priority, resp, event_data);
+        } else {
+          formUpdateBody(resp.ticket.related_tickets.parent_id, updatesFields, map, dropdownMap, priority, resp, event_data);
+        }
+      } else {
+        console.log("its a normal ticket")
+      }
     } catch (error) {
       console.error(error);
     }
@@ -78,12 +85,14 @@ function formUpdateBody(ticketIds, updatesFields, map, dropdownMap, priority, re
   body['custom_fields'] = {}
   $.each(updatesFields, function (k, v) {
     if (resp.ticket.hasOwnProperty(v)) {
-      if (v === "priority" && v !== priority)
+      if (v === "priority" && resp.ticket[v] !== priority)
         body[v] = resp.ticket[v];
       else if (v === "responder_id" && event_data["responder_id"].value !== "")
         body[v] = resp.ticket[v];
-      else if (v !== "priority" && v !== "responder_id")
+      else if (v !== "priority" && v !== "responder_id") {
+        console.log("other than priority and responder id")
         body[v] = resp.ticket[v];
+      }
     }
     else if (!resp.ticket.hasOwnProperty(v) && resp.ticket.custom_fields.hasOwnProperty(v)) {
       body.custom_fields[v] = resp.ticket.custom_fields[v];
@@ -95,23 +104,59 @@ function formUpdateBody(ticketIds, updatesFields, map, dropdownMap, priority, re
     }
   });
   console.log(body);
-  // if (Object.keys(body).length >= 1) {
-  //   if (Object.keys(body.custom_fields).length || Object.keys(body).length > 1) {
-  //     if (ticketIds.length > 0) {
-  //       var count = 0;
-  //       iterate(count, ticketIds, body, "parent", priority);
-  //     } else {
-  //       iterate(count, ticketIds, body, "child", priority);
-  //     }
-  //   }
+  if (Object.keys(body).length >= 1) {
+    if (Object.keys(body.custom_fields).length || Object.keys(body).length > 1) {
+      if (ticketIds.length > 0) {
+        var count = 0;
+        iterate(count, ticketIds, body, "parent", priority);
+      } else {
+        iterate(count, ticketIds, body, "child", priority);
+      }
+    }
 
-  // }
+  }
 
+} function iterate(count, ticketIds, body, origin) {
+  if (origin === 'child') {
+    process(count, ticketIds, body, origin);
+  } else {
+    if (count < ticketIds.length) {
+      process(count, ticketIds, body, origin);
+    }
+  }
+
+}
+function handleErr(err = 'None') {
+  showNotification("error", err);
+}
+function process(count, arr, body, origin) {
+  var headers = { "Authorization": "Basic <%= encode(iparam.apikey) %>", "Content-Type": "application/json" };
+  var options = { headers: headers, body: JSON.stringify(body) };
+  var url = (origin === 'child') ? `https://<%= iparam.domain %>/api/v2/tickets/${arr}` :
+    `https://<%= iparam.domain %>/api/v2/tickets/${arr[count]}`;
+  client.request.put(url, options).then(function () {
+    if (origin !== 'child') {
+      iterate(count + 1, arr, body, origin);
+      console.log(`Updated successfully ticket id- ${arr[count]} `);
+    }
+    if (count + 1 === arr.length || origin === 'child') {
+      showNotification("success", "Ticket(s) updated successfully");
+    }
+  }, function (error) {
+    console.log("in update block");
+    try {
+      var errResp = JSON.parse(error.response);
+      showNotification("error", errResp.errors);
+
+    } catch (error) {
+      showNotification("error", error);
+    }
+  });
 }
 
 function ticketErrorBlock(error) {
   try {
-    var errResp = JSON.stringify(error.response);
+    var errResp = JSON.parse(error.response);
     showNotification("error", errResp.message);
 
   } catch (error) {
@@ -121,7 +166,7 @@ function ticketErrorBlock(error) {
 function parseErrorBlock(error) {
   console.log("in ticket fields block");
   try {
-    var errResp = JSON.stringify(error.response);
+    var errResp = JSON.parse(error.response);
     showNotification("error", errResp.message);
 
   } catch (error) {
